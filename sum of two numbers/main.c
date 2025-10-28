f38132afc50a801f6f698810995a895e7e7adc7b
PRAJWAL <prajwalr432025@gmail.com>
2025-10-28 20:41:49 +0530

    Add hello world program

---
+++ sum of two numbers/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,sum;
+
+    printf("Enter the two numbers :");
+    scanf("%d %d",&num1,&num2);
+
+    sum = num1 + num2;
+
+    printf("The sum of numbers is : %d",sum);
+
+    return 0;
+
+}
